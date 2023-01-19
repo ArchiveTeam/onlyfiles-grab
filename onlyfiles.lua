1479c17c71dbb4bb2edd8d9064bb3154008eccb4
arkiver <arkiver@protonmail.com>
2023-01-19 07:47:49 +0100

    Fix missing html.

--- onlyfiles.lua
+++ onlyfiles.lua
@@ -215,6 +215,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
 
   if allowed(url)
     and status_code < 300 then
+    html = read_file(file)
     for newurl in string.gmatch(string.gsub(html, "&quot;", '"'), '([^"]+)') do
       checknewurl(newurl)
     end
